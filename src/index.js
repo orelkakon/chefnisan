a68395cad3354ba1741e9fdb427be744c6c554a7
orelkakon <orelkakonweb@gmail.com>
2023-02-28 22:51:01 +0200

    fix on production

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import './index.css';
 import App from './App';
 import Privacy from './Components/Privacy';
 import {
-  BrowserRouter,
   Routes,
   Route,
+  HashRouter
 } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/privacy" element={<Privacy />} />
-    </Routes>
-    </BrowserRouter>
+    <HashRouter>
+      <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/privacy" element={<Privacy />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
